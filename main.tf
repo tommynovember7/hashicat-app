7d13b69a72bd85a03108763d31a7e527a1d10951
tommynovember7 <tomo@tn7.io>
2024-01-17 07:39:14 +0000

    Added s3 bucket private module

--- main.tf
+++ main.tf
@@ -199,7 +199,7 @@ resource "aws_key_pair" "hashicat" {
 }
 
 module "s3-bucket" {
-  source              = "cloudposse/s3-bucket/aws"
+  source              = "app.terraform.io/trial_tn7_org/s3-bucket/aws"
   version             = "3.1.0"
   s3_object_ownership = "BucketOwnerEnforced"
 }
